d6e301fdb1d5288501d8c528717ea6e2e475c753
Matthew Close <matthew.close@ctl.io>
2016-03-14 16:40:34 -0500

    From page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -40,7 +40,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-                any(row.text == '1: Buy some coffee pods' for row in rows)
+                any(row.text == '1: Buy some coffee pods' for row in rows),
+                "New to-do item did not appear in table"
                 )
         
         # There is still a text box inviting me to add another item. I
