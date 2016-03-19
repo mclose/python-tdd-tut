066a597d7bb5958856eeb949e92af664c58288c2
mclose <md_close@yahoo.com>
2016-03-19 12:21:02 -0500

    added new URL, view and template to display created lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -72,11 +72,11 @@ class NewVisitorTest(LiveServerTestCase):
         # Francis starts a new list by entering a new item.
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
-        inputbox.send_keys('Keys.ENTER')
+        inputbox.send_keys(Keys.ENTER)
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, '/lists/+.')
+        self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, my_list_url)
 
         # Again, there is no trace of Edith's list
